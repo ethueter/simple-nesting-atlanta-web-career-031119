a2954bc546b5a1fd5ff463ea94b1a312656d193b
Tyler Hueter <tylerhueter08@gmail.com>
2019-03-02 19:08:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -103,7 +103,7 @@ def changing_alan
      alans_new_info = "GUI"
      
      programmer_hash[:alan_kay][:known_for]= #{alans_new_info}
-     programmer_hash[:alan_kay][:known_for]
+     programmer_hash[:alan_kay]
 end
 
 def adding_to_dennis
